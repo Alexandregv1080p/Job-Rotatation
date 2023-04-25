d3349cbbf5d84163345b0179189cb4681d9db337
Alexandregv1080p <alexandregv1999@gmail.com>
2023-04-25 00:07:46 -0300

    commit

--- Exercicio_2.java
+++ Exercicio_2.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Exercicio_2{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
         Integer num = scanner.nextInt();
 
         Integer a = 0;
@@ -14,6 +15,11 @@ public class Exercicio_2{
             c = a + b;
             a = b;
             b = c;
+        }
+        if(b == num){
+            System.out.println("O numero: "+ num + " faz parte da sequencia de fibonnaci");
+        }else{
+            System.out.println("O numero: "+ num + " não faz parte da sequencia de fibonnaci");
         }
     }
 }
---
+++ Exercicio_5.java
@@ -0,0 +1,9 @@
+public class Exercicio_5 {
+    public static void main(String[] args) {
+        String myString = "alucard";
+        for(Integer i = myString.length() - 1; i >= 0; i--){
+            System.out.print(myString.charAt(i));
+        }
+        
+    }
+}
